b052481c2f8150d05696fcfb998ff8fe12715cef
Vlad <vladislav0000054@gmail.com>
2020-11-06 18:58:09 +0700

    fix ArrayListHome project

--- CourseTasks/ArrayListHome/ArrayListHome.cs
+++ CourseTasks/ArrayListHome/ArrayListHome.cs
@@ -9,20 +9,14 @@ namespace ArrayListHome
         public static void Main()
         {
             string path = "..\\..\\Text.txt";
-            List<string> list = new List<string>();
+            List<string> stringList = GetListFromFile(path);
+            Console.WriteLine(string.Join("|", stringList));
 
-            ReadAll(path, list);
-            Console.WriteLine(list[0]);
-
-            List<int> numbersList = new List<int>()
-            {
-                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-            };
-
+            List<int> numbersList = new List<int> { 1, 2, 2, 2, 26, 5, 6, 7, 8, 9, 10 };
             RemoveEvenNumbers(numbersList);
-            Console.WriteLine(numbersList.Count);
+            Console.WriteLine(string.Join(" ", numbersList));
 
-            List<int> duplicateNumbersList = new List<int>()
+            List<int> duplicateNumbersList = new List<int>
             {
                 1, 2, 3, 4, 5,
                 1, 2, 3, 4, 5,
@@ -30,23 +24,30 @@ namespace ArrayListHome
                 6, 7, 8, 9, 0
             };
 
-            List<int> uniqueNumbersList = RemoveDuplicateNumbers(duplicateNumbersList);
+            List<int> uniqueNumbersList = GetUniqueNumbersList(duplicateNumbersList);
             Console.WriteLine(string.Join(" ", uniqueNumbersList));
         }
 
-        public static void ReadAll(string path, List<string> list)
+        public static List<string> GetListFromFile(string path)
         {
-            using (StreamReader reader = new StreamReader(path))
-            {
-                string currentLine;
+            List<string> stringList = new List<string>();
 
-                while (!reader.EndOfStream)
+            try
+            {
+                using (StreamReader reader = new StreamReader(path))
                 {
-                    if ((currentLine = reader.ReadLine()).Length != 0)
+                    while (!reader.EndOfStream)
                     {
-                        list.Add(currentLine);
+                        stringList.Add(reader.ReadLine());
                     }
                 }
+
+                return stringList;
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine($"Файл \"{e.FileName}\" не найден");
+                return stringList;
             }
         }
 
@@ -57,22 +58,21 @@ namespace ArrayListHome
                 if (numbersList[i] % 2 == 0)
                 {
                     numbersList.RemoveAt(i);
+                    i--;
                 }
             }
         }
 
-        public static List<int> RemoveDuplicateNumbers(List<int> duplicateNumbersList)
+        public static List<int> GetUniqueNumbersList(List<int> duplicateNumbersList)
         {
-            List<int> uniqueNumbersList = new List<int>();
+            List<int> uniqueNumbersList = new List<int>(duplicateNumbersList.Count);
 
-            for (int i = 0; i < duplicateNumbersList.Count; i++)
+            foreach (int number in duplicateNumbersList)
             {
-                if (uniqueNumbersList.Contains(duplicateNumbersList[i]))
+                if (!uniqueNumbersList.Contains(number))
                 {
-                    continue;
+                    uniqueNumbersList.Add(number);
                 }
-
-                uniqueNumbersList.Add(duplicateNumbersList[i]);
             }
 
             return uniqueNumbersList;
